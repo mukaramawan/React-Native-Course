44d5e5b9ba7818783308fb7350549840f15fff1a
Mukaram Awan <141765944+mukaramawan@users.noreply.github.com>
2024-10-09 12:42:37 +0500

    Basic Event Handling & UseState Concepts

--- App.js
+++ App.js
@@ -1,35 +1,60 @@
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [goals, setGoals] = useState([]);
+  function inputHandeler(enteredText) {
+    setEnteredGoal(enteredText);
+  }
+
+  function addGoalHandeler() {
+    setGoals((currentCourseGoals) => [...goals, enteredGoal])
+    console.log(goals)
+  }
+
   return (
     <View style={styles.appContainer}>
-
       <View style={styles.inputContainer}>
-        <TextInput style={styles.TextInput} placeholder='Enter a Task'/>
-        <Button title='Tap Me' />
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Enter a Task"
+          onChangeText={inputHandeler}
+        />
+
+        <Button title="Tap Me" onPress={addGoalHandeler} />
       </View>
 
-      <View>
-        <Text>List of Goals</Text>
+      <View style={styles.goalsContainer}>
+        {goals.map( (goal) => <Text key={goal}>{goal}</Text> )}
       </View>
-
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "grey",
   },
   TextInput: {
     borderWidth: 1,
-    borderColor: 'black',
-    width: '80%',
+    borderColor: "grey",
+    width: "80%",
     marginRight: 8,
     padding: 8,
-  }
+  },
+  goalsContainer: {
+    flex: 5,
+  },
 });
